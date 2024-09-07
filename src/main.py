a534a97676442f99955b51f28f7bdbebf2a4b1de
roman <carpowroma@gmail.com>
2024-09-07 18:36:32 +0300

    Create requirements.txt and parse .env

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 import json
 
-HOST = '127.0.0.1'
-PORT = 7777
-DEBUG = True
-
 def emojify(text: str) -> str:
     new_text = text
     
@@ -19,6 +15,18 @@ def emojify(text: str) -> str:
         new_text = new_text.replace(f"emoji:{emoji}", f"<img class=\"emoji_icon\" src=\"/static/img/emoji_{emoji}.png\"></img>")
     
     return new_text
+
+dotenv_values = {}
+
+with open(".env", "r") as f:
+    for line in f.readlines():
+        values = line.rstrip().split('=')
+        if len(values) == 2:
+            dotenv_values[values[0]] = values[1]
+            
+DEBUG = bool(int(dotenv_values.get("DEBUG", "0")))
+HOST = dotenv_values.get("HOST", "127.0.0.1")
+PORT = int(dotenv_values.get("PORT", 7777))
 
 app = Flask("Portfolio Website", template_folder="./templates")
 
